539caf230b0878f8d6a9f1f2f39fe1ccfedc4736
ZaraJoy <zarajoy@outlook.com>
2024-08-28 12:56:49 +1000

    add terraform CD user + policy

---
+++ infra/setup/iam.tf
@@ -0,0 +1,64 @@
+####################################################
+# Create IAM users and policies for the CD account #
+####################################################
+resource "aws_iam_user" "cd" {
+  name = "recipe-app-api-cd"
+}
+
+resource "aws_iam_access_key" "cd" {
+  user = aws_iam_user.cd.name
+}
+
+
+
+####################################################
+#Policy for TF backend to s3 & dynamodb access #
+####################################################
+
+data "aws_iam_policy_document" "tf_backend" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:ListBucket"
+    ]
+    resources = [
+      "arn:aws:s3:::${var.tf_state_bucket}"
+    ]
+
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject"
+    ]
+    resources = [
+      "arn:aws:s3:::${var.tf_state_bucket}/tf_state_deploy/*",
+      "arn:aws:s3:::${var.tf_state_bucket}/tf_state_deploy-env/*"
+    ]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "dynamodb:DescribeTable",
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:DeleteItem"
+    ]
+    resources = [
+      "arn:aws:dynamodb:*:*:table/${var.tf_state_lock_table}"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "tf_backend" {
+  name        = "${aws_iam_user.cd.name}-tf-s3-dynamodb"
+  description = "Allow user to use s3 and dynamodb for tf backend resources"
+  policy      = data.aws_iam_policy_document.tf_backend.json
+}
+
+resource "aws_iam_user_policy_attachment" "tf_backend" {
+  user       = aws_iam_user.cd.name
+  policy_arn = aws_iam_policy.tf_backend.arn
+}
