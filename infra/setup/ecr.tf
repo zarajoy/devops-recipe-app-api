30ad6644c9ad825032378fa8ddc760334e194c72
ZaraJoy <zarajoy@outlook.com>
2024-08-28 15:53:40 +1000

    set up GH actions workflows

--- infra/setup/ecr.tf
+++ infra/setup/ecr.tf
@@ -3,21 +3,21 @@
 #############################################
 
 resource "aws_ecr_repository" "app" {
-    name = "recipe-app-api-app"
-    image_tag_mutability = "MUTABLE"
-    force_delete = true
-    image_scanning_configuration {
-        # update to true when deploying prod
-        scan_on_push = false
-    }
+  name                 = "recipe-app-api-app"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+  image_scanning_configuration {
+    # update to true when deploying prod
+    scan_on_push = false
+  }
 }
 
 resource "aws_ecr_repository" "proxy" {
-    name = "recipe-app-api-proxy"
-    image_tag_mutability = "MUTABLE"
-    force_delete = true
-    image_scanning_configuration {
-        # update to true when deploying prod
-        scan_on_push = false
-    }
+  name                 = "recipe-app-api-proxy"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+  image_scanning_configuration {
+    # update to true when deploying prod
+    scan_on_push = false
+  }
 }
