c5ef739eb11c798daea5022838143aa8c65ffa34
zarajoy <zarajoy@outlook.com>
2024-08-28 13:15:25 +1000

    add ecr tf and update outputs

--- infra/setup/outputs.tf
+++ infra/setup/outputs.tf
@@ -7,5 +7,14 @@ output "cd_user_access_key_secret" {
   description = "Access key secret for CD user"
   value       = aws_iam_access_key.cd.secret
   sensitive   = true
+}
 
+output "ecr_repo_app" {
+  description = "ecr repo url for app image"
+  value       = aws_ecr_repository.app.repository_url
+}
+
+output "ecr_repo_proxy" {
+  description = "ecr repo url for proxy image"
+  value       = aws_ecr_repository.proxy.repository_url
 }
