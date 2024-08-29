64fa65de272057b424522f8ca02f97bc7f13a1db
ZaraJoy <zarajoy@outlook.com>
2024-08-29 15:53:23 +1000

    added private networks to network.tf

--- infra/deploy/network.tf
+++ infra/deploy/network.tf
@@ -3,7 +3,8 @@
 ############################
 
 resource "aws_vpc" "main" {
-  cidr_block           = "10.1.0.0/16"
+  cidr_block = "10.1.0.0/16"
+
   enable_dns_hostnames = true
   enable_dns_support   = true
 }
@@ -14,7 +15,6 @@ resource "aws_vpc" "main" {
 
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
-
   tags = {
     Name = "${local.prefix}-main"
   }
@@ -84,3 +84,28 @@ resource "aws_route" "public_internet_access_route_b" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.main.id
 }
+
+########################################
+# private subnets for internal access  #
+########################################
+
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.1.10.0/24"
+  availability_zone = "${data.aws_region.current.name}a"
+
+  tags = {
+    Name = "${local.prefix}-private-a"
+  }
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.1.11.0/24"
+  availability_zone = "${data.aws_region.current.name}b"
+
+  tags = {
+    Name = "${local.prefix}-private-b"
+  }
+}
+
