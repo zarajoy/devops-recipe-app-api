43441aeeeff0995c9b2ce1542bfd141c34273883
ZaraJoy <zarajoy@outlook.com>
2024-08-30 09:38:02 +1000

    add ecs.tf

---
+++ infra/deploy/ecs.tf
@@ -0,0 +1,8 @@
+#####################################################
+# ECS cluster for running the recipe app on fargate #
+######################################################
+
+resource "aws_ecs_cluster" "main" {
+  name = "${local.prefix}-cluster"
+  
+}
