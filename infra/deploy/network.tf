055e50a904c91b77b84ea14a7d33a561670fb03b
ZaraJoy <zarajoy@outlook.com>
2024-08-29 13:49:13 +1000

    added vpc and igw

---
+++ infra/deploy/network.tf
@@ -0,0 +1,21 @@
+############################
+# Network Infrastructure #
+############################
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.1.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+############################
+# Internet Gateway (for ALB) #
+############################
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main
+
+  tags = {
+    Name = "${local.prefix}-main"
+  }
+}
