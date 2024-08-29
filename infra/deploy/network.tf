a74c7efec0406c92d1be6d2ee005b98f3aec858a
ZaraJoy <zarajoy@outlook.com>
2024-08-29 15:24:29 +1000

    add public subnets and routes to network.tf

--- infra/deploy/network.tf
+++ infra/deploy/network.tf
@@ -19,3 +19,69 @@ resource "aws_internet_gateway" "main" {
     Name = "${local.prefix}-main"
   }
 }
+
+
+###########################################
+# public subnets for the load banlancer #
+######################################
+
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.1.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "${data.aws_region.current.name}a"
+
+  tags = {
+    Name = "${local.prefix}-public-a"
+  }
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.1.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "${data.aws_region.current.name}b"
+
+  tags = {
+    Name = "${local.prefix}-public-b"
+  }
+}
+
+resource "aws_route_table" "public-a" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "${local.prefix}-public-a"
+  }
+}
+
+resource "aws_route_table" "public-b" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "${local.prefix}-public-b"
+  }
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public-a.id
+}
+
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public-b.id
+}
+
+resource "aws_route" "public_internet_access_route_a" {
+  route_table_id         = aws_route_table.public-a.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+
+}
+
+resource "aws_route" "public_internet_access_route_b" {
+  route_table_id         = aws_route_table.public-b.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+
+}
