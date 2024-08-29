57b2ba84b76f04585c043230a5a3480935f4d10f
ZaraJoy <zarajoy@outlook.com>
2024-08-30 09:52:54 +1000

    add ecs role policy

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -2,7 +2,25 @@
 # ECS cluster for running the recipe app on fargate #
 ######################################################
 
+
+resource "aws_iam_policy" "task_execution_role_policy" {
+  name        = "${local.prefix}-task-execution-role-policy"
+  path        = "/"
+  description = "Allow ECS to retrieve images and add to cw logs"
+  policy      = file("./templates/ecs/task-execution-role-policy.json")
+}
+
+resource "aws_iam_role" "task_execution_role" {
+  name               = "${local.prefix}-task-execution-role"
+  assume_role_policy = file("./templates/ecs/task-assume-role-policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "task_execution_role" {
+  role       = aws_iam_role.task_execution_role.name
+  policy_arn = aws_iam_policy.task_execution_role_policy.arn
+}
+
+
 resource "aws_ecs_cluster" "main" {
   name = "${local.prefix}-cluster"
-  
 }
