0a8015ddd666c919edf76cd3842488fd227cc4a8
ZaraJoy <zarajoy@outlook.com>
2024-09-01 09:56:08 +1000

    added efs mount targets

--- infra/deploy/efs.tf
+++ infra/deploy/efs.tf
@@ -21,3 +21,16 @@ resource "aws_efs_file_system" "media" {
   }
 
 }
+
+resource "aws_efs_mount_target" "media_a" {
+  file_system_id  = aws_efs_file_system.media.id
+  subnet_id       = aws_subnet.private_a.id
+  security_groups = [aws_security_group.efs.id]
+
+}
+resource "aws_efs_mount_target" "media_b" {
+  file_system_id  = aws_efs_file_system.media.id
+  subnet_id       = aws_subnet.private_b.id
+  security_groups = [aws_security_group.efs.id]
+
+}
