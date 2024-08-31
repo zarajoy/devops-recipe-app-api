545bee18a812867852b21e48bdefabaa77366a42
ZaraJoy <zarajoy@outlook.com>
2024-09-01 09:49:26 +1000

    added efs

---
+++ infra/deploy/efs.tf
@@ -0,0 +1,11 @@
+################################
+# efs for media storage #######
+################################
+
+resource "aws_efs_file_system" "media" {
+  encrypted = true
+  tags = {
+    Name = "${local.prefix}-media"
+  }
+
+}
