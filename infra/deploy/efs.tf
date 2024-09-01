8d2bb6d1bb4bed0c536e5221dcfd5801d9e5f528
ZaraJoy <zarajoy@outlook.com>
2024-09-01 10:25:39 +1000

    configure ecs tasks to use efs

--- infra/deploy/efs.tf
+++ infra/deploy/efs.tf
@@ -34,3 +34,16 @@ resource "aws_efs_mount_target" "media_b" {
   security_groups = [aws_security_group.efs.id]
 
 }
+
+resource "aws_efs_access_point" "media" {
+  file_system_id = aws_efs_file_system.media.id
+  root_directory {
+    path = "/api/media"
+    creation_info {
+      owner_gid = 101
+      owner_uid = 101
+      permissions = "755"
+    }
+  }
+  
+}
